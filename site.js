8b858472fea3f4720c0069fed88d11ec2944b706
Luis Mendoza <luismendoza@Luiss-MacBook-Pro.local>
2017-11-03 20:40:25 -0700

    working code with css

---
+++ site.js
@@ -0,0 +1,106 @@
+
+//****************** Global Variables *****************//
+
+var sportsTeams = ["Barcelona", "Real Madrid", "Liverpool", "PSG", "Club America", "Lakers",];
+
+
+//****************** List of functions *****************//
+
+function showcontent() {
+      $("#gifArea").empty();
+      var tv = $(this).attr("teamName");
+      var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=IelN2EsP53lCGIzF6kjIakIkgXbSa3bL&q="
+                     + tv + "&limit=10&offset=0&rating=G&lang=en";
+
+      $.ajax({
+
+        url: queryURL,
+        method: "GET"
+
+      }).done(function(response){
+
+        jsondata = $("<div>");
+        
+        //Creating the gifs and rating for each gif
+        for (var i = 0; i < 10; i++) {
+
+          var rating = ("<p> Rating: " + response.data[i].rating + "</p>");
+          var image = $("<img>").attr("src", response.data[i].images.original_still.url);
+          image.attr("data-state","still");
+          image.attr("data-still",response.data[i].images.original_still.url);
+          image.attr("data-animate",response.data[i].images.original.url);
+
+
+          image.addClass("clicks");
+          var newDiv = $("<div>")
+          newDiv.addClass("col-xs-4 col-xs-12 gifs");
+          newDiv.append(rating, image);
+
+          $("#gifArea").append(newDiv);
+        };
+
+    });
+
+};
+
+$(document).on("click", ".clicks", function(){
+
+        var state = $(this).attr("data-state");
+
+        if (state === "still") {
+          $(this).attr("src", $(this).attr("data-animate")  );
+          $(this).attr("data-state", 'animate' );
+        }
+        else if ( state === "animate" ) {
+          $(this).attr("src", $(this).attr("data-still")  );
+          $(this).attr("data-state", 'still' )
+
+        }
+
+
+});
+
+
+
+function renderButtons() {
+
+      $("#buttonArea").empty();
+      $("#userInput").val("");
+
+      for (var i = 0; i < sportsTeams.length; i++) {
+
+            var newTeam = $("<button>");
+            newTeam.addClass("teams");
+            newTeam.attr("teamName", sportsTeams[i]);
+            newTeam.text(sportsTeams[i]);
+            $("#buttonArea").append(newTeam);
+
+      }
+
+};
+
+
+$("#addButton").on("click", function(event) {
+      
+    if( $("#userInput").val()=="" || sportsTeams.indexOf( $("#userInput").val()  )  > -1   ){
+
+      return false;
+
+    } 
+    else {
+
+        sportsTeams.push($("#userInput").val());
+        $("#buttonArea").append(sportsTeams);
+        event.preventDefault();
+        renderButtons();
+
+    }    
+
+
+});
+
+$(document).on("click", ".teams", showcontent);
+
+renderButtons();
+
+
